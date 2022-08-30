58916d6d0441a545b857e640f5e81c01f9bd7b7f
Rom <rom@kraken.git>
2022-08-30 11:19:32 +0200

    Table to redirect to other pages other than just the applications, the comment to have a kinda different look

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-export const Comment = ({content, created_by, action_name}) => (
+export const Comment = ({content, createdBy, actionName, actionDetails, createdAt}) => (
     <div style={{ margin: '', marginBottom: '' }} >
-        <p style={{ fontWeight: '900', margin: '0 0 16px 0' }} >{created_by}: {action_name != 'COMMENT' && action_name}</p>
-        <p style={{ color: '#777' }} >{content}</p>
+        <div>
+            <p style={{ fontWeight: '900', margin: '0 0 16px 0', display: 'inline-block' }} >{createdBy}</p> <span>{actionDetails}</span>
+        </div>
+        <div>
+            <span style={{ fontWeight: '300' }} >{createdAt}</span>
+        </div>
+        {
+            content && <p style={{ color: '#777', border: '1px solid #eee', borderRadius: '8px' }} >{content}</p>
+        }
     </div>
 )
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { Table as AntTable } from 'antd'
 
-export const Table = ({dataSource, columns, toStep = true, empty_icon, navigate}) => {
+export const Table = ({dataSource, columns, toStep = true, empty_icon, navigate, prefix = 'application/', suffix = ''}) => {
   return (
     (dataSource || []).length ? 
-    <AntTable tableLayout="auto" size='large' dataSource={dataSource} columns={columns} scroll={{ y: 300 }} onRow={({id, current_step}, _) => ({onClick: _ => navigate(`/application/${id}/${toStep ? current_step : ''}`) })}/> :
+    <AntTable tableLayout="auto" size='large' dataSource={dataSource} columns={columns} scroll={{ y: 300 }} onRow={({id, current_step}, _) => ({onClick: _ => navigate(`/${prefix}${id}${suffix}`) })}/> :
     <div className='fill-height grid'>
         <p style={{ textAlign: 'center' }} ><img src={empty_icon} width='140px' /><br/>No applications to show!</p>
     </div>
