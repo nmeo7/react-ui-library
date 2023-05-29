421740eef7e30710e59a6e352fe023a8466861bb
Rom <rom@kraken.git>
2023-05-29 15:08:54 +0200

    .

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,12 +2,33 @@ import React from 'react'
 
 import { Table as AntTable } from 'antd'
 
-export const Table = ({dataSource, columns, toStep = true, empty_icon, navigate, prefix = 'application/', suffix = () => ''}) => {
-  return (
-    (dataSource || []).length ? 
-    <AntTable tableLayout="auto" size='large' dataSource={dataSource} columns={columns} scroll={{ y: 300 }} onRow={({id, ...row}, _) => ({onClick: _ => navigate(`/${prefix}${id}${suffix(row)}`) })}/> :
-    <div className='fill-height grid'>
-        <p style={{ textAlign: 'center' }} ><img src={empty_icon} width='140px' /><br/>No Items to show!</p>
+export const Table = ({
+  dataSource,
+  columns,
+  toStep = true,
+  empty_icon,
+  navigate,
+  prefix = 'application/',
+  suffix = () => ''
+}) => {
+  return (dataSource || []).length ? (
+    <AntTable
+      tableLayout='auto'
+      size='large'
+      dataSource={dataSource}
+      columns={columns}
+      scroll={{ y: 300 }}
+      onRow={({ id, ...row }, _) => ({
+        onClick: (_) => navigate(`/${prefix}${id}${suffix(row)}`)
+      })}
+    />
+  ) : (
+    <div className='grid' style={{}}>
+      <p style={{ textAlign: 'center' }}>
+        <img src={empty_icon} width='140px' />
+        <br />
+        No Items to show!
+      </p>
     </div>
   )
 }
